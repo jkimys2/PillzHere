75493151a0946c63bc709b387def03fd73cf1145
Cody T <crthorn11@gmail.com>
2024-04-27 12:18:46 -0400

    Added in appropriate exports and variables for index in models, created user model/schema

---
+++ server/models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    userEmail: {
+        type: String,
+        required: true
+    },
+    pillz: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        VitaminD: Number,
+        Advil: Number,
+        Amoxicillin: Number,
+        Prednisone: Number,
+        Lisinopril: Number,
+        Ibuprofen: Number,
+        Atenolol: Number,
+        Mucinex: Number,
+        Hydrochlorothiazide: Number
+    },
+    passwordUser: {
+        type: String,
+        required: true,
+    },
+    refreshToken: String
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+// {
+//     "first_name": "Paul",
+//     "last_name": "Miller",
+//     "email": "paulmiller1@example.com",
+//     "password": "Supersecure56",
+//     "pillz": [
+//         {
+//             "pill": "Tylenol",
+//             "dosage": "200mg"
+//         },
+//         {
+//             "pill": "aceteminophin",
+//             "dosage": "250mg"
+//         }
+//     ]
+// }
