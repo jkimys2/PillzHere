ad248d4f9f4a266ac310c7eb6ccde25c1aaba71f
Cody T <crthorn11@gmail.com>
2024-04-29 19:10:58 -0400

    Updated serverjs file added folders for type defs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,62 +1,42 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// Require model
-//const {  } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   const newGenre = new ({ name: req.params.genre });
-//   newGenre.save();
-//   if (newGenre) {
-//     res.status(200).json(newGenre);
-//   } else {
-//     console.log('Uh Oh, something went wrong');
-//     res.status(500).json({ message: 'something went wrong' });
-//   }
-// });
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const result = await Genre.find({});
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log('Uh Oh, something went wrong');
-//     res.status(500).json({ message: 'something went wrong' });
-//   }
-// });
-
-// app.get('/', async (req, res) => {
-//   try {
-//     const result = await Genre.findOne({ name: 'Kids' });
-//     res.status(200).json(result);
-//   } catch (err) {
-//     console.log('Uh Oh, something went wrong');
-//     res.status(500).json({ message: 'something went wrong' });
-//   }
-// });
-
-// app.delete('/', async (req, res) => {
-//   try {
-//     const result = await .findOneAndDelete({ name: req.params.genre });
-//     res.status(200).json(result);
-//     console.log(`Deleted: ${result}`);
-//   } catch (err) {
-//     console.log('Uh Oh, something went wrong');
-//     res.status(500).json({ message: 'something went wrong' });
-//   }
-// });
-
-// app.put('/', async (req, res) => {
-
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+  });
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
 });
+};
+
+startApolloServer();
