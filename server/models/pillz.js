93b10f92d417561518c60dc95fd4599a5b2ec26e
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-04-29 19:06:55 -0400

    Update pillz.js

--- server/models/pillz.js
+++ server/models/pillz.js
@@ -1,32 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const pillzSchema = new mongoose.Schema({
-  
-//   pillz: { type: String, required: true },
-//   pillzCount: Number,
-//   dosage: Number,
-//   reminder: Boolean,
-//   // Use built in date method to get current date
-//   lastAccessed: { type: Date, default: Date.now },
-// });
-
-
-// const Pillz = mongoose.model('pillz', pillzSchema);
-
-// const handleError = (err) => console.error(err);
-
-// Pillz
-//   .create({
-//     pillz: 'Aceteminophin',
-//     pillzCount: 2,
-//     dosage: 250,
-//     reminder: true,
-//   })
-//   .then(result => console.log('Created new pill', result))
-//   .catch(err => handleError(err));
-
-// module.exports = Pillz;
-
 const {Schema, model} = require('mongoose');
 
 const pillzSchema = new Schema({
