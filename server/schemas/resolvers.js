630bbe8b94efa79e4f5a71af7a569390e5cb37f6
Cody T <crthorn11@gmail.com>
2024-04-29 21:27:01 -0400

    Finsihed user in TypeDef, started pillz in typedef, started resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,18 @@
-const { pillz } = require('../models');
+const { pillz, user } = require('../models');
+const user = require('../models/user');
+
+const resolvers = {
+    Query: {
+      user: async () => {
+        return user.find().populate('user');
+      },
+    }
+
+//   Mutation: {
+//     addUser: async (parent, { username, email, password }) => {
+//       const user = await User.create({ username, email, password });
+//       const token = signToken(user);
+//       return { token, user };
+//     },
+//   },
+//  };
