c6aca14a7d9b33ad05a71c53905f480c09a515a5
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-04-29 21:30:53 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,7 @@
 const routes = require('express').Router();
 const userRoutes = require('./userRoutes');
-const pillzRoutes = require('./pillzRoutes');
+
 
 routes.use('/users', userRoutes);
-routes.use('/pillz', pillzRoutes);
 
 module.exports = routes;
