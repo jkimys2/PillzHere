8849f7bd8770593dee4c6e4a6c373b24f44cef6a
Cody T <crthorn11@gmail.com>
2024-04-30 20:52:10 -0400

    Handled an error in TypeDef resolver, imported auth into server and changed path names

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-// const express = require('express');
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -14,28 +15,31 @@ const server = new ApolloServer({
   resolvers,
 });
 
-const startApolloServer = async () => {
-  await server.start();
-  
-  app.use('/graphql', expressMiddleware(server));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client')));
-  
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
-  });
-}
+const startApolloServer = async () => {
+  await server.start();
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  app.use('/graphql', expressMiddleware(server, { context: authMiddleware }));
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/index.html')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
-});
 };
 
 startApolloServer();
