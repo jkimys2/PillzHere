23e16e90a3d9dc4e79333adf3575771931ecaea7
Jordan Newhouse <jkimys2@gmail.com>
2024-04-30 20:00:37 -0400

    deleted stuffs

--- client/src/components/PillForm.jsx
+++ client/src/components/PillForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { CREATE_USER } from "../utils/mutations";
-import { createUser } from "../utils/API";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 
