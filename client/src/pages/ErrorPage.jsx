308d4842105c0fb8bb68c23d21683bf0c2dd60ef
nicholasstacy89 <116467600+nicholasstacy89@users.noreply.github.com>
2024-05-02 19:05:51 -0400

    Update ErrorPage.jsx

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,14 +1,14 @@
 import { useRouteError } from "react-router-dom";
-
+import sadLouisImage from "../assets/images/sadlouis.png";
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
     <div id="error-page">
-        <img src= "./assets/images/sadlouis.png" alt="Louis is sad" />
+        <img src={sadLouisImage} alt="Louis is sad"/>
       <h1>Louis is sad</h1>
-      <p>There are no Pillz Here!</p>
+      <h4>There are no Pillz Here!</h4>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
