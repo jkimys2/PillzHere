905718756dcf54180ae5daf4e4f19aa765a8735e
Cody T <crthorn11@gmail.com>
2024-05-02 20:20:17 -0400

    Fixed an error where a resolver was defined but was left out of the schema, corrected resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,74 +1,67 @@
-const Pillz = require('../models/pillz');
-const User = require('../models/user');
-const { pillz, user } = require('../models');
+const { User, Pillz } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-      users: async () => {
-        return User.find().populate('pillz');
-      },
-      user: async (parent, { username }) => {
-        return User.findOne({ username }).populate('pillz');
-      },
-      //It is "pillzs" with an s because mongoDB does not know the z is implied plural. It actually helps in our favor.
-      pillzs: async (parent, { username }) => {
-        const params = username ? { username } : {};
-        return Pillz.find(params).sort({ createdAt: -1 });
-      },
-      pillz: async (parent, { pillzId }) => {
-        return Pillz.findOne({ _id: pillzId });
-      },
-      me: async (parent, args, context) => {
-        if (context.user) {
-          return User.findOne({ _id: context.user._id }).populate('pillz');
-        }
-        throw AuthenticationError;
-      },
+  Query: {
+    users: async () => {
+      return User.find().populate('pillzs');
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('pillzs');
+    },
+    //It is "pillzs" with an s because mongoDB does not know the z is implied plural. It actually helps in our favor.
+    pillz: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Pillz.find(params).sort({ createdAt: -1 });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('pillz');
+      }
+      throw AuthenticationError;
+    },
+  },
 
-Mutation: {
+  Mutation: {
     addUser: async (parent, { username, email, password }) => {
-        const user = await User.create({ username, email, password });
-        const token = signToken(user);
-        return { token, user };
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-  
-        if (!user) {
-          throw AuthenticationError;
-        }
-  
-        const correctPw = await user.isCorrectPassword(password);
-  
-        if (!correctPw) {
-          throw AuthenticationError;
-        }
-  
-        const token = signToken(user);
-  
-        return { token, user };
-      },
-      addPillz: async (parent, { pillz }, context) => {
-        if (context.user) {
-          const newPillz = await Pillz.create({
-            pillz,
-            username: context.user.username,
-          });
-  
-          await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { pillz: pillz._id } }
-          );
-  
-          return pillz;
-        }
-        throw AuthenticationError;
-        ('You need to be logged in!');
-      },
+      const user = await User.findOne({ email });
 
-   }
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addPillz: async (parent, { Pillz }, context) => {
+      if (context.user) {
+        const pillz = await Pillz.create({
+          Pillz,
+          username: context.user.username,
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { pillzs: pillz._id } },
+          { new: true, runValidators: true },
+        );
+
+        return pillz;
+      }
+      throw AuthenticationError;
+    },
+  },
 };
 
 module.exports = resolvers;
