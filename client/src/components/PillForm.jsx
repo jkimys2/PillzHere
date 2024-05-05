1b77fbeb1be1c265ed1d292a1bbc934d0fc56d39
Jordan Newhouse <jkimys2@gmail.com>
2024-05-04 23:34:11 -0400

    pill form style added - looking good

--- client/src/components/PillForm.jsx
+++ client/src/components/PillForm.jsx
@@ -1,5 +1,6 @@
+import "./PillForm.css";
 import { useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
+import { Form, Button, Alert, Container } from "react-bootstrap";
 import { ADD_PILLZ } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
@@ -15,8 +16,9 @@ const pillForm = () => {
   });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [addPillz] = useMutation(ADD_PILLZ, {refetchQueries: [QUERY_USER, "me"]});
-
+  const [addPillz] = useMutation(ADD_PILLZ, {
+    refetchQueries: [QUERY_USER, "me"],
+  });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,16 +27,13 @@ const pillForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    // const form = event.currentTarget;
-    // if (form.checkValidity() === false) {
-    //   // event.preventDefault();
-    //   event.stopPropagation();
-    // }
-
     try {
-     await addPillz({ variables: { ...userFormData, quantity: parseInt(userFormData.quantity) } });
-      // Auth.login(data.addPillz.token);
+      await addPillz({
+        variables: {
+          ...userFormData,
+          quantity: parseInt(userFormData.quantity),
+        },
+      });
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -50,7 +49,7 @@ const pillForm = () => {
   };
 
   return (
-    <>
+    <Container className="cont">
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
         <Alert
           dismissible
@@ -60,9 +59,11 @@ const pillForm = () => {
         >
           Something went wrong with adding your Pillz!
         </Alert>
+
         <Form.Group>
-          <Form.Label htmlFor="name">Pillz Name</Form.Label>
+          <Form.Label className="label" htmlFor="name">Pillz Name:</Form.Label>
           <Form.Control
+            className="form"
             type="text"
             placeholder="Pillz Name"
             name="name"
@@ -70,14 +71,15 @@ const pillForm = () => {
             value={userFormData.name}
             required
           />
-           <Form.Control.Feedback type="invalid">
+          <Form.Control.Feedback type="invalid">
             Pillz Name is required!
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor="quantity">Pillz Quantity</Form.Label>
+          <Form.Label className="label" htmlFor="quantity">Pillz Quantity:</Form.Label>
           <Form.Control
+            className="form"
             type="text"
             placeholder="Pillz Quantity"
             name="quantity"
@@ -85,14 +87,15 @@ const pillForm = () => {
             value={userFormData.quantity}
             required
           />
-           <Form.Control.Feedback type="invalid">
+          <Form.Control.Feedback type="invalid">
             Pillz Quantity is required!
           </Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group>
-          <Form.Label htmlFor="dosage">Pillz Dosage</Form.Label>
+          <Form.Label className="label" htmlFor="dosage">Pillz Dosage:</Form.Label>
           <Form.Control
+            className="form"
             type="text"
             placeholder="Pillz Dosage"
             name="dosage"
@@ -100,27 +103,23 @@ const pillForm = () => {
             value={userFormData.dosage}
             required
           />
-           <Form.Control.Feedback type="invalid">
+          <Form.Control.Feedback type="invalid">
             Pillz Dosage is required!
           </Form.Control.Feedback>
         </Form.Group>
-        
+
         <Button
+        className="button"
           disabled={
-            !(
-              userFormData.name &&
-              userFormData.quantity &&
-              userFormData.dosage
-            )
+            !(userFormData.name && userFormData.quantity && userFormData.dosage)
           }
           type="submit"
           variant="success"
         >
           Add Pillz!
         </Button>
-        
       </Form>
-    </>
+    </Container>
   );
 };
 
