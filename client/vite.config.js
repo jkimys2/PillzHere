be4e230a3c79f5935eb04623e57b464f0eae4f28
Jordan Newhouse <jkimys2@gmail.com>
2024-05-06 10:58:08 -0400

    pwa is finally workinggit add -A!

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: "/public/manifest.json",
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
   server: {
     port: 3000,
     open: true,
